0632881e9df9f0685064755bdcb4ca81f6fb58b4
drupal-roadie <ihartchicago@gmail.com>
2015-02-06 18:10:56 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>title</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="robots" content="index,follow" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+
+<body >
+
+<div class ="container">
+
+<h1 class ="bg-info text-danger"> Welcome to a basic website</h1>
+	<div class="row bg-info">
+	<div class="col-sm-4">hello1</div>
+	<div class="col-sm-4">hello2</div>
+	<div class="col-sm-4">hello3</div>
+	</div>
+
+	<div class ="row">
+	<div class="col-sm-4">qargqregqregreqgqergergreg Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.</div>
+	
+	<div class="col-sm-4">qargqregqregreqgqergergreg Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.hello2</div>
+	<div class="col-sm-4">qargqregqregreqgqergergreg Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.</div>
+	</div>
+	
+	
+	<form name="?" method="get" action="?" enctype="application/x-www-form-urlencoded">
+	Name
+	<input type="text" name="name" value="" placeholder="enter name" id="?" maxlength="20" />
+	<br/>
+	Email
+	<input type="text" name="email" value="" placeholder="enter email"	id="?" maxlength="30" />
+	<br/>
+	
+	<br/>
+	Mouse<input type="checkbox" name="dog" id="?" value="dog" checked="checked"/> <br/>
+	Squirrel<input type="checkbox" name="" id="?" value="?" checked="checked" />
+	<input type="button" name="" id="?" value="Button" />
+	
+	</form>
+<h2>This is a little smaller heading </h2>
+
+<p>qargqregqregreqgqergergreg Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.Aenean dignissim felis sed odio sodales viverra. Integer eget faucibus lorem. Integer rutrum felis a arcu suscipit dignissim. Sed eu eleifend mauris, at ornare enim. Morbi egestas leo elit, quis pulvinar sapien varius at! Fusce facilisis orci ut nisi facilisis, non venenatis lacus feugiat! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Suspendisse quis nisl vulputate, finibus dui ut, consequat orci. Suspendisse leo orci; convallis sit amet sapien vel, consequat suscipit metus? Donec interdum elit maximus dictum ultricies. Cras id lacinia magna, eget dapibus quam. Quisque in nibh non nunc feugiat fermentum? Nullam a turpis nec dui luctus dignissim? Cras luctus nisl in rutrum maximus? Donec placerat ultricies nisi sed laoreet. Integer a massa finibus, condimentum ex a, varius lacus. Suspendisse iaculis, libero sit amet egestas sollicitudin, mauris tellus rhoncus eros, in iaculis ligula nisl eu odio. Ut maximus, nisi vitae efficitur scelerisque, nisi leo bibendum nisl; imperdiet porttitor sem augue eu nisl. Nunc eget purus a justo consectetur pulvinar sit amet sed odio.
+
+Duis facilisis dignissim neque in vehicula? Phasellus tempus consequat leo. Mauris fermentum arcu cras amet.
+
+ </p>
+
+</div>
+
+</body>
+</html>
